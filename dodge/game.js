ea96a88ab1f7f1a1d6d841fbf7a8faf46f12abb4
katie <katielattaa@gmail.com>
2016-05-10 03:20:14 +0000

    sped up player and enemy

--- dodge/game.js
+++ dodge/game.js
@@ -25,18 +25,18 @@ function draw() {
     background(backgroundImage);
     
     if (keyDown(RIGHT_ARROW) && player.position.x < (width-25)) {
-    player.position.x = player.position.x + 10;
+    player.position.x = player.position.x + 35;
     }
     if (keyDown(LEFT_ARROW) && player.position.x > 25) {
-    player.position.x = player.position.x - 10;
+    player.position.x = player.position.x - 35;
     }
     if (keyDown(UP_ARROW) && player.position.y > 25) {
-    player.position.y = player.position.y - 10;
+    player.position.y = player.position.y - 35;
     }
     if (keyDown(DOWN_ARROW) && player.position.y < height-25) {
-    player.position.y = player.position.y + 10;
+    player.position.y = player.position.y + 35;
     }
-    enemy.position.y = enemy.position.y + 15;
+    enemy.position.y = enemy.position.y + 30;
     if (enemy.position.y > height) {
         enemy.position.y = 0;
         enemy.position.x = random(5, width-5)
