1bb69cf1b8e8d75ac63c517252ffbaebd5fac48b
katie <katielattaa@gmail.com>
2016-05-31 01:56:01 +0000

    made chat

---
+++ chat/main.js
@@ -0,0 +1,34 @@
+var config = {
+    apiKey: "AIzaSyAMear2CASfHVXIKo9y_qQzfVvkhZ9M9BQ",
+    authDomain: "chat-app-d15cc.firebaseapp.com",
+    databaseURL: "https://chat-app-d15cc.firebaseio.com",
+    storageBucket: "",
+};
+firebase.initializeApp(config);
+var chatData = firebase.database().ref();
+
+function pushMessage(event) {
+    if (event.keyCode == 13) {
+        var name = $('#nameInput').val();
+        var text = $('#messageInput').val();
+        chatData.push({name: name, text: text});
+        $('#messageInput').val('');
+    }
+}
+chatData.on("child_added", showMessage);
+
+function showMessage(msg) {
+    var message = msg.val();
+    var messageSender = message.name;
+    var messageContent = message.text;
+    
+    var messageEl = $("<div/>").addClass("message");
+    var senderEl = $("<span/>").text(messageSender + ": ");
+    var contentEl = $("<span/>").text(messageContent);
+    
+    messageEl.append(senderEl);
+    messageEl.append(contentEl);
+    $('#messages').append(messageEl);
+}
+
+$('#messageInput').keypress(pushMessage);
